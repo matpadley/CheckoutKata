a96d4c451a324e894df75edf5da559ca83d04e76
Mat Padley <mrpadley@gmail.com>
2020-04-13 12:01:27 +0100

    Failing A99 Special Offer Test

--- CheckoutKata.Tests/SpecialOfferTests.cs
+++ CheckoutKata.Tests/SpecialOfferTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
 using NUnit.Framework;
 
 namespace CheckoutKata.Tests
@@ -8,6 +9,16 @@ namespace CheckoutKata.Tests
         public void Setup()
         {
             base.Setup();
+        }
+
+        [Test]
+        public void Assert_Can_Apply_Special_Offer_For_A99()
+        {
+            Checkout.Scan(A99);
+            Checkout.Scan(A99);
+            Checkout.Scan(A99);
+            
+            Assert.AreEqual(1.3, Checkout.Total);
         }
     }
 }
