178060bb257a22079a004a14e4187ce05d26d548
Mat Padley <mrpadley@gmail.com>
2020-04-13 12:06:25 +0100

    Moved special offer object to class lib

--- CheckoutKata.Tests/SpecialOfferTests.cs
+++ CheckoutKata.Tests/SpecialOfferTests.cs
@@ -35,18 +35,4 @@ namespace CheckoutKata.Tests
         }
         
     }
-
-    public class SpecialOffer
-    {
-        public string Sku { get; }
-        public int Quantity { get; }
-        public double OfferPrice { get; }
-
-        public SpecialOffer(string sku, in int quantity, in double offerPrice)
-        {
-            Sku = sku;
-            Quantity = quantity;
-            OfferPrice = offerPrice;
-        }
-    }
 }
---
+++ CheckoutKata/SpecialOffer.cs
@@ -0,0 +1,16 @@
+namespace CheckoutKata
+{
+    public class SpecialOffer
+    {
+        public string Sku { get; }
+        public int Quantity { get; }
+        public double OfferPrice { get; }
+
+        public SpecialOffer(string sku, in int quantity, in double offerPrice)
+        {
+            Sku = sku;
+            Quantity = quantity;
+            OfferPrice = offerPrice;
+        }
+    }
+}
