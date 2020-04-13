fc39c5f97d63562eb84a6a473ef1991a4102f97f
Mat Padley <mrpadley@gmail.com>
2020-04-13 11:37:07 +0100

    Failing Assert To Scan Item

--- CheckoutKata.Tests/ScanItemTests.cs
+++ CheckoutKata.Tests/ScanItemTests.cs
@@ -4,9 +4,30 @@ namespace CheckoutKata.Tests
 {
     public class ScanItemTests
     {
+        private Checkout _checkout;
+        
         [SetUp]
         public void Setup()
         {
+            _checkout = new Checkout();
+        }
+
+        [Test]
+        public void Assert_Can_Add_Item_At_Checkout()
+        {
+            Assert.DoesNotThrow(() => _checkout.Scan(new Item()));
+        }
+    }
+
+    public class Item
+    {
+    }
+
+    internal class Checkout
+    {
+        public void Scan(Item item)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
