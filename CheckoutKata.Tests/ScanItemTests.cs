e43a5154a1c78b1ae44c643eb54208f1708986e2
Mat Padley <mrpadley@gmail.com>
2020-04-13 11:43:31 +0100

    Added Item details

--- CheckoutKata.Tests/ScanItemTests.cs
+++ CheckoutKata.Tests/ScanItemTests.cs
@@ -12,16 +12,35 @@ namespace CheckoutKata.Tests
         {
             _checkout = new Checkout();
         }
-
         [Test]
         public void Assert_Can_Add_Item_At_Checkout()
         {
-            Assert.DoesNotThrow(() => _checkout.Scan(new Item()));
+            Assert.DoesNotThrow(() => _checkout.Scan(new Item(string.Empty, 0)));
+        }
+
+        [TestCase("A99", 0.5)]
+        [TestCase("B15", 0.3)]
+        [TestCase("C40", 0.6)]
+        public void Assert_Item_Can_Be_Created(string sku, double unitPrice)
+        {
+            var item = new Item(sku, unitPrice);
+            
+            Assert.AreEqual(sku, item.Sku);
+            
+            Assert.AreEqual(unitPrice, item.UnitPrice);
         }
     }
 
     public class Item
     {
+        public string Sku { get; }
+        public double UnitPrice { get; }
+
+        public Item(string sku, double unitPrice)
+        {
+            Sku = sku;
+            UnitPrice = unitPrice;
+        }
     }
 
     internal class Checkout
